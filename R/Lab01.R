a41c906d2339ae4cd6d3a6174aa7e58f964fc8fb
Nick Bentelspacher <nspacher@vt.edu>
2023-01-25 00:38:18 +0000

    cleaned code, added comments

--- R/Lab01.R
+++ R/Lab01.R
@@ -6,12 +6,6 @@ print("hello world version 3")
 system("git config --global user.email 'nspacher@vt.edu' ") 
 system("git config --global user.name 'Nick Bentelspacher' ")
 
-data <- data.frame(
-  day = as.Date("2019-01-01") + 0:99,
-  tmin = runif(100) + seq(1,100)^2.5 / 10000,
-  price = runif(100) + seq(100,1)^1.5 / 10
-)
-
 source("https://goo.gl/Cb8zGn") #function for getting usgs data
 myflowgage_id="01668000"
 myflowgage=get_usgs_gage(myflowgage_id,
@@ -51,8 +45,8 @@ WXData <- WXData %>% mutate(tmin=tmin/10,
 
 #HW1 weather plot
 precipColor <- "#009E73"
-start <- ymd("2023/01/01")
-end <- ymd("2023/01/14")
+start <- ymd("2022/11/01")
+end <- ymd("2023/01/01")
 
 weather_plot <- WXData %>% filter(date>start, date<end) %>% ggplot(aes(x=date))+
   geom_col(aes(y=prcp, fill="Precipitation"), alpha = 0.6)+
@@ -81,7 +75,7 @@ HW2_w <- WXData %>% filter(date>start, date<end) %>% ggplot(aes(x=date))+
   geom_line(aes(y=tmin, color="Minimum Temperature"))+
   geom_line(aes(y=tmax, color="Maximum Temperature"))+
   geom_line(data=myflowgage$flowdata %>% filter(mdate>start, mdate<end),
-            aes(mdate, flow/100000, color="Flow"))+
+            aes(mdate, flow/1000000, color="Flow"))+
   scale_y_continuous(
     # Features of the first axis
     name = "Temperature (C)",
@@ -102,38 +96,3 @@ HW2_plot <- wrap_elements(get_plot_component(streamflow_plot, "ylab-l"))+
 
 HW2_plot
 
-
-# data$tmax = runif(100)*10  + data$tmin
-# coeff <- 6
-# # A few constants
-# tminColor <- "#0000ff"
-# tmaxColor <- "#ff0000"
-# priceColor <- rgb(0.2, 0.6, 0.9, 1)
-# dir.create("pdfs")
-# basestr=format(Sys.time(),"./pdfs/%Y%m%d%H%M")
-# 
-# p1= ggplot(data, aes(x=day)) +
-#   geom_line( aes(y=tmin), linewidth=2, color=tminColor) + 
-#   geom_line( aes(y=tmax), linewidth=2, color=tmaxColor) + 
-#   geom_line( aes(y=price / coeff), linewidth=2, color=priceColor) +
-#     scale_y_continuous(
-#       # Features of the first axis
-#       name = "Temp(C)",
-#       
-#       # Add a second axis and specify its features
-#       sec.axis = sec_axis(~.*coeff, name="Price")
-#     ) + 
-#     # theme_ipsum() +
-#     theme(
-#       axis.title.y = element_text(color = "black", size=13),
-#       axis.title.y.right = element_text(color = priceColor, size=13)
-#     ) +
-#     ggtitle("Temperature down, price up")
-# 
-# filename=paste0(basestr,"graph01.pdf")
-# pdf(filename) 
-# plot(p1)
-# dev.off()
-# print("file size")
-# print(file.size(filename))
-# print("I finished!")

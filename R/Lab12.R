50b8fbd4ccd439eb392e1b7302a7544e4a2c4606
nspacher <nspacher@vt.edu>
2023-04-13 14:36:47 -0400

    Lab12

---
+++ R/Lab12.R
@@ -0,0 +1,209 @@
+setwd("~")
+objects()
+rm(list=objects())
+url="https://raw.githubusercontent.com/vtdrfuka/BSE5304Labs/main/R/MySetup.R"
+download.file(url,"DanSetup.R")
+file.edit("DanSetup.R")
+# Open Bignette and then come back to start package install
+# because it takes a while to install multisensi
+#
+url="https://cran.r-project.org/web/packages/multisensi/vignettes/multisensi-vignette.pdf"
+browseURL(url)
+pacman::p_load(data.table,multisensi)
+# KEEP OPEN AS YOU WILL BE WALKING THROUGH IT FOR LAB	
+vignette("multisensi-vignette")
+
+verhulst <- function(K, Y0, a, t) {
+  output <- K/(1 + (K/Y0 - 1) * exp(-a * t))
+  return(output)
+}
+
+T <- seq(from = 5, to = 100, by = 5)
+verhulst2 <- function(X, t = T) {
+  out <- matrix(nrow = nrow(X), ncol = length(t), NA)
+  for (i in 1:nrow(X)) {
+    out[i, ] <- verhulst(X$K[i], X$Y0[i], X$a[i], t)
+  }
+  out <- as.data.frame(out)
+  names(out) <- paste("t", t, sep = "")
+  return(out)
+}
+
+n <- 10
+set.seed(1234)
+X <- data.frame(K = runif(n, min = 100, max = 1000), Y0 = runif(n, min = 1,
+                                                                max = 40), a = runif(n, min = 0.05, max = 0.2))
+Y <- verhulst2(X)
+par(cex.axis = 0.7, cex.lab = 0.8)
+plot(T, Y[1, ], type = "l", xlab = "Time", ylab = "Population size",
+     ylim = c(0, 1000))
+for (i in 2:n) {
+  lines(T, Y[i, ], type = "l", col = i)
+}
+
+
+verhulst.seq <- multisensi(model=verhulst2, reduction=NULL, center=FALSE,
+                           design.args = list( K=c(100,400,1000), Y0=c(1,20,40), a=c(0.05,0.1,0.2)))
+
+# color palettes: rainbow, heat.colors, terrain.colors, topo.colors,
+# cm.colors
+plot(verhulst.seq, normalized = TRUE, color = terrain.colors, gsi.plot = FALSE)
+title(xlab = "Time in half-decades.")
+plot(verhulst.seq, normalized = FALSE, color = terrain.colors, gsi.plot = FALSE)
+title(xlab = "Time in half-decades.")
+
+print(verhulst.seq, digits = 2)
+
+X <- expand.grid(K=c(100,400,1000), Y0=c(1,20,40), a=c(0.05,0.1,0.2))
+Y <- verhulst2(X) ## this part can be performed outside R if necessary
+verhulst.seq <- multisensi(design=X, model=Y, reduction=NULL, center=FALSE)
+
+
+verhulst.pca <- multisensi(design=X, model=Y, reduction=basis.ACP, scale=FALSE)
+summary(verhulst.pca, digits = 2)
+
+plot(verhulst.pca, graph = 1)
+
+plot(verhulst.pca, graph = 2)
+
+verhulst.poly <- multisensi(design = X, model = Y, reduction = basis.poly,
+                            dimension = 0.99, center = FALSE, scale = FALSE, cumul = FALSE,
+                            basis.args = list(degree=6, x.coord=T), analysis = analysis.anoasg,
+                            analysis.args = list(formula=2, keep.outputs=FALSE))
+summary(verhulst.poly, digits=2)
+plot(verhulst.poly, nb.comp = 3, graph = 1)
+
+verhulst.bspl <- multisensi(design=X, model=Y, reduction=basis.bsplines,
+                            dimension=NULL, center=FALSE, scale=FALSE,
+                            basis.args=list(knots=10, mdegree=3), cumul=FALSE,
+                            analysis=analysis.anoasg,
+                            analysis.args=list(formula=2, keep.outputs=FALSE))
+plot(verhulst.bspl, nb.comp = 5, graph = 1)
+
+library(sensitivity)
+m <- 10000
+Xb <- data.frame(K = runif(m, min = 100, max = 1000), Y0 = runif(m, min = 1,
+                                                                 max = 40), a = runif(m, min = 0.05, max = 0.2))
+verhulst.seq.sobol <- multisensi(design = sobol2007, model = verhulst2,
+                                 reduction = NULL, analysis = analysis.sensitivity, center = TRUE,
+                                 design.args = list(X1 = Xb[1:(m/2), ], X2 = Xb[(1 + m/2):m, ], nboot = 100),
+                                 analysis.args = list(keep.outputs = FALSE))
+## [*] Design
+## [*] Response simulation
+## [*] Analysis + Sensitivity Indices
+print(verhulst.seq.sobol, digits = 2)
+
+plot(verhulst.seq.sobol, normalized = TRUE, color = terrain.colors, gsi.plot = FALSE)
+title(xlab = "Time in half-decades")
+
+verhulst.seq.fast <- multisensi(design = fast99, model = verhulst2,
+                                center = FALSE, reduction = NULL, analysis = analysis.sensitivity,
+                                design.args=list( factors=c("K","Y0","a"), n=1000, q = "qunif",
+                                                  q.arg = list(list(min=100, max=1000), list(min=1, max=40),
+                                                               list(min = 0.05, max = 0.2))),
+                                analysis.args=list(keep.outputs=FALSE))
+## [*] Design
+## [*] Response simulation
+## [*] Analysis + Sensitivity Indices
+print(verhulst.seq.fast,digits=2)
+
+?EcoHydRology::Solar
+
+J <- seq(from = 1, to = 365, by = 5)
+# Solar(lat, Jday, Tx, Tn, albedo=0.2, forest=0, slope=0, aspect = 0,
+#      units="kJm2d")
+# Note that the EcoHydRology::Solar() function is for specific days, 
+# as such, we will want to create a function to loop through our period
+# of interest:
+Solar_Looped <- function(X, Jday = J) {
+  out <- matrix(nrow = nrow(X), ncol = length(Jday), NA)
+  for (i in 1:nrow(X)) {
+    out[i, ] <- Solar(lat=X$lat[i],
+                      Jday=Jday, Tx=X$Tx[i], 
+                      Tn=(X$Tx[i]-X$Trange[i]), 
+                      X$slope[i],X$aspect[i],units="Wm2")
+  }
+  out <- as.data.frame(out)
+  names(out) <- paste("Jday", Jday, sep = "")
+  return(out)
+}
+
+n <- 10
+set.seed(1234)
+X <- data.frame(Tx = runif(n, min = 5, max = 30), Trange = runif(n, min = 2,max = 16), 
+                slope = runif(n, min = 0.0, max = 0.2),
+                aspect = runif(n, min = 0.0, max = 0.2),
+                lat=runif(n, min = 0.0, max = 1.1))  # 1.1 radians lat is where?
+
+Y <- Solar_Looped(X,Jday = J)
+
+par(cex.axis = 0.7, cex.lab = 0.8)
+plot(J, Y[1, ], type = "l", xlab = "Day of Year", 
+       ylab = "Surface Short Wave Rad(W/m^2)")
+for (i in 2:n) {
+  lines(J, Y[i, ], type = "l", col = i)
+}
+
+Solar_Looped.seq <- multisensi(model=Solar_Looped, reduction=NULL, center=FALSE,
+                                 design.args = list( Tx = c(5,15,25), 
+                                                     Trange = c(2,9,16), 
+                                                     slope = c(0.1,0.2,0.3),
+                                                     aspect = c(0.1,.5,1.0),
+                                                     lat=c(0.1,.77,1.1)))
+
+print(Solar_Looped.seq, digits = 2)
+#
+# 3.2 Graphical representation of sensitivity indices
+#
+dev.off() # Clean up previous par()
+plot(Solar_Looped.seq, normalized = TRUE, color = terrain.colors, gsi.plot = FALSE)#normalized the upper subplot shows the extreme (tirets), #inter-quartile (grey) and median (bold line) output values
+title(xlab = "Days of the Year.")
+plot(Solar_Looped.seq, normalized = FALSE, color = terrain.colors, gsi.plot = FALSE)
+title(xlab = "Days of the Year.")
+
+
+# 4 Multivariate sensitivity analysis based on PCA
+# read the vignette, though note we are using the multisensi() function
+# to run our model (i.e. no “design” variable, and model=Solar_Looped)
+Solar_Looped.pca <- multisensi(model=Solar_Looped, reduction=basis.ACP, scale=FALSE,
+                                 design.args = list( Tx = c(5,15,25), 
+                                                     Trange = c(2,9,16), 
+                                                     slope = c(0.1,0.2,0.3),
+                                                     aspect = c(0.1,.5,1.0),
+                                                     lat=c(0.1,.77,1.1)))
+
+summary(Solar_Looped.pca, digits = 2)
+# 4.2 Graphical representation for PCA based analysis with 
+# explanation in vignette. These graphs require the plot window to be larger
+# and might give "Error in plot.new() : figure margins too large". 
+# If so expand the plot window.
+dev.off()
+plot(Solar_Looped.pca, graph = 1)
+plot(Solar_Looped.pca, graph = 2)
+plot(Solar_Looped.pca, graph = 3)
+#
+# 5.1 Polynomial reduction of the multivariate output
+# Skip 5.1 Polynomial reduction for now and move on to
+# 6 Alternative methods of sensitivity analysis
+# 6.1 With Sobol2007 implemented in the package sensitivity
+# 
+library(sensitivity)
+m <- 10000
+Xb <- data.frame(Tx = runif(m, min = 5, max = 30), 
+                   Trange = runif(m, min = 2,max = 16), 
+                   slope = runif(m, min = 0.0, max = 0.2),
+                   aspect = runif(m, min = 0.0, max = 0.2),
+                   lat=runif(m, min = 0.0, max = 1.1))
+
+Solar_Looped.seq.sobol <- multisensi(design = sobol2007, model = Solar_Looped,
+                                       reduction = NULL, analysis = analysis.sensitivity, center = TRUE,
+                                       design.args = list(X1 = Xb[1:(m/2), ], X2 = Xb[(1 + m/2):m, ], nboot = 100),
+                                       analysis.args = list(keep.outputs = FALSE))
+#
+# Note, this is a good time time get a drink of water and/or pee as 
+# it is running the function m=10,000 times (a few minutes).
+#
+print(Solar_Looped.seq.sobol, digits = 2)
+dev.off()
+plot(Solar_Looped.seq.sobol, normalized = TRUE, color = terrain.colors)
+

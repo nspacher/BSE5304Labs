d10f906cd2aa5fd4524c5ec202e56a38d48f395f
nspacher <nspacher@vt.edu>
2023-05-04 21:34:38 -0400

    plots etc

--- R/AlignHistWithForecast.R
+++ R/AlignHistWithForecast.R
@@ -1,5 +1,5 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(meteoForecast)
+pacman::p_load(meteoForecast,EcoHydRology,tidyverse,zoo)
 
 gfsvarstemp <- grepVar('temp', service='gfs', complete = T)
 testDay <- Sys.Date() - 1
@@ -11,7 +11,8 @@ library(lattice)
 # file.edit("Lab04_10.R")
 
 today=Sys.Date()
-myflowgage_id="0205551460"  # Old Friendly Gage
+#myflowgage_id="0205551460"  # Old Friendly Gage
+myflowgage_id="01668000"
 myflowgage=get_usgs_gage(myflowgage_id,begin_date = "2015-01-01",
                          end_date = today)
 
@@ -21,18 +22,28 @@ Precipvars <- getPoint(c(myflowgage$declon, myflowgage$declat),
                  vars = "Total_precipitation_surface_Mixed_intervals_Accumulation", 
                  day = today, service='gfs')
 GFSPrecip <- aggregate(Precipvars, as.Date(time(Precipvars)), sum) #in mm 
+GFSPrecip <- fortify.zoo(GFSPrecip)
 
 Tempvars <- getPoint(c(myflowgage$declon, myflowgage$declat), 
                        vars = "Temperature_surface", 
                        day = today, service='gfs')
 GFSMaxTemp <- aggregate(Tempvars, as.Date(time(Tempvars)), max) #temp in kelvin 
+GFSMaxTemp <- fortify.zoo(GFSMaxTemp) %>% mutate(tempc=GFSMaxTemp-273)
 GFSMinTemp <- aggregate(Tempvars, as.Date(time(Tempvars)), min)
+GFSMinTemp <- fortify.zoo(GFSMinTemp) %>% mutate(tempc=GFSMinTemp-273)
 
+#test_df <- fortify.zoo(GFSMaxTemp)
 
-plot(precip)
+ggplot(GFSPrecip,aes(Index,GFSPrecip))+
+  geom_line()+
+  labs(y="Precipitation (mm)",x=element_blank(),title="Forecasted Precipitation")
 
-plot(GFSMinTemp,ylim=c(276,300))
-lines(GFSMaxTemp)
+ggplot(GFSMinTemp,aes(Index,tempc,color="Min. Temp."))+
+  geom_line()+
+  geom_line(data=GFSMaxTemp,aes(y=tempc,color="Max. Temp."))+
+  scale_color_hue(name=element_blank())+
+  labs(y="Temperature (Celsius)",x=element_blank(),title="Forecasted Temperature")
+
 
 myflowgage$flowdata$Qmm = myflowgage$flowdata$flow/myflowgage$area/10^3
 source("https://raw.githubusercontent.com/Rojakaveh/FillMissWX/main/FillMissWX.R")
